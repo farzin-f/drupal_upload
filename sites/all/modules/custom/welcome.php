8ac764c15c05e691b269afa82099ccb11866c8d0
Ubuntu <ubuntu@ip-172-31-15-238.ca-central-1.compute.internal>
2018-07-30 03:33:30 +0000

    the whole project

---
+++ sites/all/modules/custom/welcome.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col-md-4">
+	<div class="panel panel-default">
+	    <div class="panel-heading">Une architecture technologique entièrement refondue</div>
+	    <div class="panel-body">
+		DiliTrust Data Room bénéficie des dernières avancées technologiques et offre ainsi une puissance de chargement des données et une rapidité de restitution inégalées. Multiplateformes, la solution a été repensée « mobile first » et est disponible également sur tablettes et smartphones.
+	    </div>
+	</div>
+
+	<div class="col-md-4">
+	    <div class="panel panel-default">
+		<div class="panel-heading">Une sécurité renforcée adaptée à un environnement hostile</div>
+		<div class="panel-body">
+		    Dans un contexte économique complexe et agressif, il est difficile de mener ses transactions B2B ou ses due diligence sans se protéger d’une éventuelle fuite ou d’un piratage. A cet effet Dilitrust est hébergé en France, hors Freedom Act (ex Patriot Act). De nouvelles fonctions de sécurité ont été développées, telles que l’authentification forte et la détection d’intrusions ou d’attaques diverses.
+		</div>
+	    </div>
+	</div>
+
+	<div class="col-md-4">
+	    <div class="panel panel-default">
+		<div class="panel-heading">Une interface entièrement retravaillée</div>
+		<div class="panel-body">
+		    L’ergonomie de l’application, mais également son esthétique, ont été revues en profondeur. L’interface propose une arborescence épurée permettant aux acheteurs d’accéder facilement aux documents, ainsi qu’un espace de travail collaboratif intuitif : annotations des documents, questions/réponses, partage de mémos, alertes instantanées. Les administrateurs disposent d’une palette d’outils enrichis pour accéder à des statistiques relatives à la consultation des différentes informations disponibles dans la data room.
+		</div>
+	    </div>
+
+	</div>
